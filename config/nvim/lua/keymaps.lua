d626ac9166d97573636e50b1a4016bd331049d32
fadlidevs <fadlidev99@gmail.com>
2025-06-03 01:32:22 +0700

    update lualine

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -3,5 +3,6 @@ local opts = {noremap = true,silent = true}
 
 vim.g.mapleader = " "
 map("n", "<leader>w", ":w<CR>", opts)
+map("n", "<leader>q", ":q<CR>", opts)
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
 
--- config/nvim/lua/lualine-conf.lua
+++ config/nvim/lua/lualine-conf.lua
@@ -1,19 +1,17 @@
--- File: lua/lualine-config.lua
-
-require("lualine").setup({
+require('lualine').setup {
   options = {
-    theme = "auto",      -- tema otomatis (ikut colorscheme)
-    section_separators = { left = "", right = "" },
-    component_separators = { left = "", right = "" },
     icons_enabled = true,
+    theme = 'auto',
+    component_separators = '|',
+    section_separators = '',
   },
   sections = {
-    lualine_a = { "mode" },
-    lualine_b = { "branch", "diff" },
-    lualine_c = { "filename" },
-    lualine_x = { "encoding", "fileformat", "filetype" },
-    lualine_y = { "progress" },
-    lualine_z = { "location" }
-  },
-})
+    lualine_a = {'mode'},
+    lualine_b = {''},
+    lualine_c = {'filename'},
+    lualine_x = {},
+    lualine_y = {''},
+    lualine_z = {'branch','diff'}
+  }
+}
 
