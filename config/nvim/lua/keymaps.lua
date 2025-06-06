bebe980784c7ce7423990279512d2381a5894e5b
F28-none <fadlidev99@gmail.com>
2025-06-07 01:19:22 +0700

    reset all to rewrite all

--- config/nvim/lua/keymaps.lua
+++
@@ -1,8 +0,0 @@
-local map = vim.keymap.set
-local opts = {noremap = true,silent = true}
-
-vim.g.mapleader = " "
-map("n", "<leader>w", ":w<CR>", opts)
-map("n", "<leader>q", ":q<CR>", opts)
-map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
-
