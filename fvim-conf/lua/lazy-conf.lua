2303df966764b3e2e761fad76763df1392043c98
F28-none <fadlidev99@gmail.com>
2025-06-08 16:39:22 +0700

    add plugin trreseter

--- fvim-conf/lua/lazy-conf.lua
+++ fvim-conf/lua/lazy-conf.lua
@@ -8,6 +8,19 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
+    {
+  'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+    require'nvim-treesitter.configs'.setup {
+   --   ensure_installed = { "markdown", "markdown_inline" },
+      highlight = { enable = true },
+      indent = { enable = true },
+      fold = { enable = true },
+    }
+  end
+    },
+
     --For statusline
   {
   "F28-none/statusL",
