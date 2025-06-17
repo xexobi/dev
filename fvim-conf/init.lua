5d3590b9e6759f9e081f8b67c72c680a40c5a5dc
F28-none <fadlidev99@gmail.com>
2025-06-18 02:36:07 +0700

    edit pyline

--- fvim-conf/init.lua
+++ fvim-conf/init.lua
@@ -1,4 +1,3 @@
-
 --Configuration plugins
 --=====================
 
@@ -6,28 +5,35 @@ vim.api.nvim_create_user_command("Setings", function()
   vim.cmd("cd ~/.config/nvim")
   vim.cmd("edit init.lua")
 end, {})
+
 require('lazy-conf') --for plugins manager
 require('telescope-conf') --for Lsp
 require('keymaps-conf') --for keymaps
 require('options-conf') --for options
 require('autopairs-conf')-- for autopairs
 require('lsp-conf') --for Lsp
--- Lua:
+-- Lua=
 vim.cmd[[colorscheme dracula]]
 
 vim.fn.Py_line_config({
-    file_bg = '#e0601b', -- baground color file
-    pipe= '••', -- baground color file
-    branch_bg = '#1b70e0',-- baground color file
+--     icon_mode = '#', -- icon for mode 
+--     border_left = '∆', -- baground color file
+--     border_right = '§', -- baground color file
+--     icon_branch = '', -- baground color file
+--     icon_file = '#', -- baground color file
+--     file_bg = '#e0601b', -- baground color file
+--     pipe= '•', -- baground color file
+--     branch_bg = '#1b70e0',-- baground color file
     --mode baground harus tabel lua
     mode_bg = {
-        ['\x16']= '#e01b3c', -- for visual block mode
-        n = '#09e644', -- for normal mode
-        i = '#82e609',-- for insert mode
-        v = '#09e6bd',-- for visual mode
-        V = '#e609cf',-- for visual line mode
-        c = '#e62709',-- for command mode
-        R = '#e62709',-- for replace mode
-        t = '#1b53e0',-- for terminal mode
+       Normal='#6f03fc',
+       Insert='#000000',
+       Visual='#888888',
+       VisualLine='#888888',
+       VisualBlock='#888888',
+       Command='#000000',
+       Replace='#888888',
+       Terminal='#888888',
+       Select='#888888',
     },
 })
