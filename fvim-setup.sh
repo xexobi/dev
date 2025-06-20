94002ec8eaee9126a6dee01262fbf2944fd7c727
F28-none <fadlidev99@gmail.com>
2025-06-20 22:06:50 +0700

    add plugin dan status tmux theme

--- fvim-setup.sh
+++ fvim-setup.sh
@@ -5,3 +5,4 @@ echo "alias fv=nvim" >> ~/.zshrc #for zsh
 
 ln -sf ~/dev/fvim-conf ~/.config/nvim
 ln -sf ~/dev/tmux-conf ~/.tmux.conf
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
