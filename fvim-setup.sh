9544a32768bf04167bf18245b711a082353e45bc
F28-none <fadlidev99@gmail.com>
2025-06-28 23:41:08 +0700

    rename void-install

--- fvim-setup.sh
+++
@@ -1,9 +0,0 @@
-echo "alias fv=nvim" >> ~/.bashrc # for bash
-echo "alias fv=nvim" >> ~/.zshrc #for zsh
-
-#make symlink to ~/.config/nvim
-
-ln -sf ~/dev/fvim-conf ~/.config/nvim
-ln -sf ~/dev/tmux-conf ~/.tmux.conf
-mkdir ~/.tmux/plugins/tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
