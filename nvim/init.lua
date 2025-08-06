613ff4218549ebcb7b527f9b1e70152ec610df09
xexobi <fadlidev99@gmail.com>
2025-08-07 02:27:15 +0700

    adding configuration fo ghostty

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,7 +12,7 @@ require('options-conf') --for options
 require('autopairs-conf')-- for autopairs
 require('lsp-conf') --for Lsp
 --===============================================================
-vim.cmd[[colorscheme tokyonight-storm]]
+vim.cmd[[colorscheme tokyonight-night]]
 --===============================================================
 
 -- vim.fn.Py_line_config({
