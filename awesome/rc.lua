648b132d4ebd6f13ec5a2c62044e55911f6462d4
xexobi <fadlidev99@gmail.com>
2025-08-14 02:18:41 +0700

    just commit litle

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -212,10 +212,12 @@ awful.screen.connect_for_each_screen(function(s)
         },
         s.mytasklist, -- Middle widget
         { -- Right widgets
+            mytextclock,
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
             wibox.widget.systray(),
-            mytextclock,
+            s.mylayoutbox,
+            s.mylayoutbox,
         },
     }
 end)
