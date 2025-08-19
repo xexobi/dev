bfc0ecd62e0faf97a4f48b8fd178a5f3cdbc2693
xexobi <fadlidev99@gmail.com>
2025-08-19 14:02:46 +0700

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
 function t
     tmux attach -t main; or tmux new -s main
 end
+
+set -x MOZ_ENABLE_WAYLAND 1
+set -x LANG en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
