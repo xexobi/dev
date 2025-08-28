f7ff7fdaaaf49d1e67d43ab94c36c818583938d4
xexobi <fadlidev99@gmail.com>
2025-08-28 11:02:15 +0700

    dev

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -1,19 +1,58 @@
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●", -- simbol di depan pesan
+    spacing = 2,  -- jarak dari teks kode
+  },
+    signs = {
+      text = {
+              [vim.diagnostic.severity.ERROR] = " ",
+              [vim.diagnostic.severity.WARN] = " ",
+              [vim.diagnostic.severity.INFO] = "󰋼 ",
+              [vim.diagnostic.severity.HINT] = "󰌵 ",
+      },
+      texthl = {
+              [vim.diagnostic.severity.ERROR] = "Error",
+              [vim.diagnostic.severity.WARN] = "Error",
+              [vim.diagnostic.severity.HINT] = "Hint",
+              [vim.diagnostic.severity.INFO] = "Info",
+      },
+      numhl = {
+              [vim.diagnostic.severity.ERROR] = "",
+              [vim.diagnostic.severity.WARN] = "",
+              [vim.diagnostic.severity.HINT] = "",
+              [vim.diagnostic.severity.INFO] = "",
+      },
+        },
+  underline = true,      -- garis bawah teks error
+  update_in_insert = true, -- tetap tampil meski lagi mengetik
+  severity_sort = true,  -- urutkan error > warning > info > hint
+})
 local lspconfig = require("lspconfig")
 local capabilities = require("blink.cmp").get_lsp_capabilities()
-
 vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
 
-lspconfig.lua_ls.setup {
-  capabilities = capabilities,
+local project_dir = vim.fn.getcwd()
+local runtime_paths = vim.api.nvim_get_runtime_file("", true)
+local filtered_paths = {}
+
+for _, path in ipairs(runtime_paths) do
+  if (path:match("/share/nvim/") or path:match("/site/pack/") or path:match(project_dir)) then
+    table.insert(filtered_paths, path)
+  end
+end
+
+lspconfig.lua_ls.setup({
+    capabilities = capabilities,
   settings = {
     Lua = {
       runtime = { version = "LuaJIT" },
-      diagnostics = { globals = { "vim","require" } },
+      diagnostics = { globals = { "vim" } },
       workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false,
+        library = filtered_paths,
+        checkThirdParty = false, -- matikan scanning library pihak ketiga
       },
+      telemetry = { enable = false },
     },
   },
-}
+})
 
